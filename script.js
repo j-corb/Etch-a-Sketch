d196a82cbec4ed75a7d9f75007677d303211ec19
jesse <corbinjesse95@gmail.com>
2022-07-07 18:38:29 -0400

    load functioning boards with varying grid dimensions

--- script.js
+++ script.js
@@ -10,10 +10,46 @@ for (i=0; i<256; i++) {
 
 
 
-const canvas = document.querySelectorAll('.block');
+function sizeSelector() {
+    size = Number(prompt('How many columns would you like?'));
+    //container.style.cssText = `repeat(${size}, 1fr)`;
+    if (size < 101) {
+        var canvas = document.querySelectorAll('.block');
+        canvas.forEach((block) => {
+            block.remove();
+        });
+      
+        for (i=0; i< size**2; i++) {
+            var grid = document.createElement('div');
+            grid.className = 'block';
+            grid.classList.add(`${i}`);
+            grid.textContent = `${i}`;
+
+            container.appendChild(grid);
+            
+        }
+        canvas = document.querySelectorAll('.block');
+
+        canvas.forEach((block) => { 
+            block.addEventListener("mouseover", () => {
+                block.classList.toggle('colorful');
+            });
+        });
+    } else {
+        alert("Please select a number less than 101 for the sake of the computer.")
+    }
+}
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', sizeSelector);
+
+var canvas = document.querySelectorAll('.block');
 
 canvas.forEach((block) => { 
     block.addEventListener("mouseover", () => {
         block.classList.toggle('colorful');
     });
 });
+
+
+
